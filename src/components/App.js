a79df7a8907df1d34f239354a4e6739cba550504
outdoorted <outdoorted@hotmail.com>
2021-02-08 14:11:28 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.header}>BakeSale App</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 40,
+  },
+});
+
+export default App;
